2c504b005ba26e6be9235b78e25d605b241a784f
zaikunzhang <zaikunzhang@gmail.com>
2024-02-25 10:27:12 +0800

    test bobyqa: maxfun = huge(maxfun) - 1

--- fortran/bobyqa/bobyqb.f90
+++ fortran/bobyqa/bobyqb.f90
@@ -32,7 +32,7 @@ module bobyqb_mod
 !
 ! Started: February 2022
 !
-! Last Modified: Wednesday, February 07, 2024 AM10:23:41
+! Last Modified: Sunday, February 25, 2024 AM10:26:03
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -633,12 +633,14 @@ if (DEBUGGING) then
     call assert(size(xhist, 1) == n .and. size(xhist, 2) == maxxhist, 'SIZE(XHIST) == [N, MAXXHIST]', srname)
     call assert(.not. any(is_nan(xhist(:, 1:min(nf, maxxhist)))), 'XHIST does not contain NaN', srname)
     ! The last calculated X can be Inf (finite + finite can be Inf numerically).
-    write(16,*) nf, maxxhist
-    do k = 1, min(huge(nf)-10000_IK, min(nf, maxxhist))
-        write(16,*) k, xhist(:, k)
-        write(16,*) xl
-        write(16,*) xu
-        write(16,*) xhist(:, k) >= xl .and. xhist(:, k) <= xu
+    write (16, *) nf, maxxhist
+    !do k = 1, min(huge(nf)-10000_IK, min(nf, maxxhist))
+    !do k = 1, min(huge(nf)-1_IK, min(nf, maxxhist))
+    do k = 1, min(nf, maxxhist)
+        write (16, *) k, xhist(:, k)
+        write (16, *) xl
+        write (16, *) xu
+        write (16, *) xhist(:, k) >= xl .and. xhist(:, k) <= xu
         call assert(all(xhist(:, k) >= xl) .and. all(xhist(:, k) <= xu), 'XL <= XHIST <= XU', srname)
     end do
     call assert(size(fhist) == maxfhist, 'SIZE(FHIST) == MAXFHIST', srname)
@@ -647,7 +649,7 @@ if (DEBUGGING) then
     call assert(.not. any(fhist(1:min(nf, maxfhist)) < f), 'F is the smallest in FHIST', srname)
 end if
 
-close(16)
+close (16)
 
 end subroutine bobyqb
 
